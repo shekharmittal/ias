bed57a1027bd0df7a24618b3694d60a92aeae6c3
Shekhar <shekhar.mittal@gmail.com>
2012-06-12 20:47:35 -0400

    June 12: All code works and files dont include comments: everything is final

--- code/createtxt2csv.pl
+++ code/createtxt2csv.pl
@@ -4,8 +4,7 @@
 use strict;
 use warnings;
 
-
-# step 1: Create a hash of arrays for hte constituencies
+# step 1: Create a hash of arrays for the constituencies
 open (FILE, '../data/constituencies.csv');
 #Fill everything into a hash
 my %hash;
@@ -14,70 +13,43 @@ while (<FILE>){
      my ($const, $state) = split(":");
      my $value = $hash{"$const"};
      if ($value){
-         print "ERROR: Hash already exists:$const:$state:$hash{$const} \n";      }
-     #$hash{$const} .= exists $hash{$const} ? "$state" : $state;
+         print "ERROR: Hash already exists:$const:$state:$hash{$const} \n";           }
     $hash{$const} = [] unless exists $hash{$const};
     push($hash{$const}, $state);
 }
 close (FILE);
-#my $i=0;
-#while ( my ($key, $value) = each(%hash) ) {
-#        print "$key=>$value\n";
-#        $i++;
-#    }
-#print "The number of keys are $i\n";
-open (FILE, '../data/LokSabhaWinners1980.txt');
+open (FILE, '../data/loksabhawinners1984.txt');
 
 #Now start matching the constituencies to the hash and writing it into a text fil
 open (ERRORFILE, '>../data/errorlog.txt');
 open (CSVFILE, '>../data/MPs.txt');
- 
 while (<FILE>){
     chomp($_);
-#    print " the string is $_\n";
     if ($_ =~/^\D/){
-#        print "ERROR: in place 1 \n";
 	print ERRORFILE "$_\n";
 	next;
     }
     elsif ($_ =~/^\d+\..*\d/){
-#        print "ERROR: in place 2 \n";
         print ERRORFILE "$_\n";
     }elsif($_ =~/^\d+\./){
-#         $_=~/(\([\w]*\)).$/;
-#        $_=~/^\d+\.\s([\w]+)(.*)(\s)([\w\(\)]*)$/;
-        $_=~/^\d+\.\s([\w]+)(.*)\s([\w\)\(]*).$/;
-       if ($3){
-            print "NO ERROR: in place 3, key value 1 is |$3| \n";
-       }else{
-           print "ERROR: in place 3, key value 1 is NULL \n";
-       }
+        $_=~/^\d+\.\s([\w]+)(.*)\s([\w\)\(]*)$/;
         my $key1 = $1;
         if (exists $hash{$key1}){
-#            print "NO ERROR: in place 5 \n";
             my $value = shift($hash{$key1});
-        #   my $value = $hash{"$key1"};
-        #   if($value){print "$key1 => $value\n";}
             if ($value){
-#                print "In the first hash function \n ";
                 print CSVFILE "$1,$value,$2,$3\n";
                 next;
             }
 	}
-        $_=~/^\d+\.\s([\w]+\s[\(\)\w]*)(.*)\s([\w\(\)]*).$/;
-#        print "NO ERROR: in place 6, key value is $1 \n";
-#        print "$1\n";
+        $_=~/^\d+\.\s([\w]+\s[\(\)\w]*)(.*)\s([\w\(\)]*)$/;
         my $key2 = $1;
         if (exists $hash{$key2}){
-#            print "NO ERROR: in place 7 \n";
             my $value1 = shift($hash{$key2});
             if ($value1){ 
-#                print "In the second hash function \n ";
                 print CSVFILE "$1,$value1,$2,$3\n";
                 next;
         	}
 	}
-#        print "ERROR: in place 4 \n";
         print ERRORFILE "$_\n";
     }
 }
